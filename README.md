e4901b68ce018c3e5feb1c00324df9c6b9036efd
Beta Lin <sb2828sb@gmail.com>
2024-01-09 20:13:19 +0800

    basic line bot structure

---
+++ README.md
@@ -0,0 +1,7 @@
+#施工中
+目標：
+1. 整合openAI的工具ChatGPT, whisper等到LINE裡面使用，作為個人助理，自動化處理一些簡單的任務
+2. line-bot-sdk已有整合openai相關套件，研究如何使用
+
+練習：測試AWS EC2的服務部署，把line bot 放到雲端VM上跑，並且持續開發和更新功能。
+測試：速度和成本
---
+++ app.py
@@ -0,0 +1,67 @@
+from dotenv import load_dotenv
+from flask import Flask, request, abort
+from dotenv import load_dotenv
+import os
+
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
+app = Flask(__name__)
+# 載入.env檔案中的環境變數
+load_dotenv()
+
+# 取得.env檔案中的環境變數
+line_channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+line_channel_secret = os.getenv('LINE_CHANNEL_SECRET')
+
+configuration = Configuration(access_token=line_channel_access_token)
+handler = WebhookHandler(line_channel_secret)
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
+
+if __name__ == "__main__":
+    app.run(debug=True)
